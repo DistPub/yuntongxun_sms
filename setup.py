95a4c636ce7e29e1d8de02a2e4576c58cfc58be6
Smite Chow <xiaopengyou2no1@gmail.com>
2016-11-25 15:23:41 +0800

    release 2.7.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='yuntongxun_sms',
     description='python sdk to use yuntongxun sms service',
     url='https://github.com/ugoodspeed/yuntongxun_sms',
-    version='2.7.0',
+    version='2.7.1',
 
     platforms='linux',
     classifiers=[
